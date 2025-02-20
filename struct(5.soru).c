93691c20ba9eb90d9efd020dbd445b14f611d60d
Ranaey9 <eyrana002@gmail.com>
2025-02-21 00:13:36 +0300

    Kullanıcıdan doğum tarihi ve şimdiki tarihi gün,ay,yıl şeklinde ayrı ayrı aldıktan sonra her iki tarih verisini YasHesapla isimli fonksiyona göndererek kullanıcının yaşını gün,ay,yıl olarak yazdırınız.

--- struct(5.soru).c
+++ struct(5.soru).c
@@ -1,36 +1,37 @@
-// Kullanıcıdan doğum tarihi ve şimdiki tarihi gün,ay,yıl şeklinde ayrı ayrı 
-// aldıktan sonra her iki tarih verisini YasHesapla isimli fonksiyona göndererek 
-// kullanıcının yaşını gün,ay,yıl olarak yazdırınız.
 #include <stdio.h>
 struct dogumtarihi
 {
- int gun;
- int ay;
- int yil; 
+    int gun;
+    int ay;
+    int yil;
 };
 struct simdikitarih
 {
-   int simdiki_gun;
-   int simdiki_ay;
-   int simdiki_yil;
+    int simdiki_gun;
+    int simdiki_ay;
+    int simdiki_yil;
 };
-int yashesapla(struct dogumtarihi dogum, struct simdikitarih simdiki){
+int yashesapla(struct dogumtarihi dogum, struct simdikitarih simdiki)
+{
     int yas = simdiki.simdiki_yil - dogum.yil;
-    if (dogum.ay>simdiki.simdiki_ay)
+    if (dogum.ay > simdiki.simdiki_ay)
     {
-       yas--;
+        yas--;
     }
-    
-return yas;
+
+    return yas;
 }
-int main(){
-     struct dogumtarihi dogum1;
-     struct simdikitarih tarih;
-     printf("doğum tarihini gir:");
-     scanf("%d%d%d",&dogum1.gun,&dogum1.ay,&dogum1.yil);
-     printf("simdiki tarihi gir");
-     scanf("%d%d%d",&tarih.simdiki_gun,&tarih.simdiki_ay,&tarih.simdiki_yil);
-     int sonuc=yashesapla(dogum1, tarih);
-printf("sonuc:%d",sonuc);
+int main()
+{
+    struct dogumtarihi dogum1;
+ 
+    struct simdikitarih tarih;
+ 
+    printf("doğum tarihini gir:");
+    scanf("%d%d%d", &dogum1.gun, &dogum1.ay, &dogum1.yil);
+    printf("simdiki tarihi gir");
+    scanf("%d%d%d", &tarih.simdiki_gun, &tarih.simdiki_ay, &tarih.simdiki_yil);
+    int sonuc = yashesapla(dogum1, tarih);
+    printf("sonuc:%d", sonuc);
     return 0;
 }
