726862f0b89ae090e2e231823f05d611ac65ef51
Ranaey9 <eyrana002@gmail.com>
2025-02-21 00:25:50 +0300

    Bir banka için müşteri hesap bilgileri(müşteri adı,bakiyesi) kaydedilecektir. Kullanıcıdan alınan n sayısına göre n adet müşteriyi pointer kullanarak oluşturunuz ve her müşterinin adını ve bakiyesini kullanıcıdan alınız. HesapListele fonksiyonunu kullanarak bankadaki müşterilerin adını ve bakiyesini listeledikten sonra tüm müşterilerin toplam bakiyesini yazdırınız.

--- struct(7.soru).c
+++ struct(7.soru).c
@@ -1,38 +1,48 @@
-// Bir banka için müşteri hesap bilgileri(müşteri adı,bakiyesi) kaydedilecektir. Kullanıcıdan alınan
-// n sayısına göre n adet müşteriyi pointer kullanarak oluşturunuz ve her müşterinin adını ve bakiyesini kullanıcıdan alınız.
-// HesapListele fonksiyonunu kullanarak bankadaki müşterilerin adını ve bakiyesini listeledikten sonra tüm müşterilerin toplam bakiyesini yazdırınız.
 #include <stdio.h>
 #include <stdlib.h>
+
 struct hesapbilgileri
 {
     char ad[50];
     int bakiye;
 };
+
 void hesapliste(int n, struct hesapbilgileri *musteri)
 {
-    int sum = 0;
-    printf("\n Müşteri Listesi\n");
+    int toplamBakiye = 0;
+    printf("\n--- Müşteri Listesi ---\n");
+    
     for (int i = 0; i < n; i++)
     {
-        sum += (musteri + i)->bakiye;
-        printf("Müşteri Adı: %s\nBakiye: %d\n", (musteri + i)->ad, (musteri + i)->bakiye);
+        toplamBakiye += musteri[i].bakiye;
+        printf("Müşteri Adı: %s\nBakiye: %d\n\n", musteri[i].ad, musteri[i].bakiye);
     }
-    printf("\nToplam bakiye: %d\n", sum);
+    
+    printf("Toplam Bakiye: %d\n", toplamBakiye);
 }
+
 int main()
 {
     int n;
-    printf("n adet sayısını gir: ");
+    printf("Kaç müşteri kaydedilecek? ");
     scanf("%d", &n);
-    struct hesapbilgileri *musteri = (struct hesapbilgileri *)malloc(sizeof(struct hesapbilgileri));
+    struct hesapbilgileri *musteri = (struct hesapbilgileri *)malloc(n * sizeof(struct hesapbilgileri));
+
+    if (musteri == NULL) 
+    {
+        printf("Bellek tahsisi başarısız!\n");
+        return 1;
+    }
     for (int i = 0; i < n; i++)
     {
-        printf("ad gir");
-        scanf("%s", (musteri + i)->ad);
-        printf("bakiye gir");
-        scanf("%d", &(musteri + i)->bakiye);
+        printf("\n%d. Müşteri Adı: ", i + 1);
+        scanf("%s", musteri[i].ad);
+        
+        printf("%d. Müşteri Bakiyesi: ", i + 1);
+        scanf("%d", &musteri[i].bakiye);
     }
     hesapliste(n, musteri);
     free(musteri);
+
     return 0;
 }
